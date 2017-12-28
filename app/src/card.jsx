ccbc3e6632c79ab4fce1a826e9c8ebcee3422d60
ajliss <aaron@lissismore.com>
2017-12-28 14:54:27 -0800

    Early jsx skeleton.

---
+++ app/src/card.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      placeholder = null
+    }
+  }
+
+  render() {
+    return (
+      <div> This is a card </div>
+    )
+  }
+
+}
---
+++ app/src/main.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Card from './card';
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      placeholder = null
+    }
+  }
+
+  render() {
+    return (
+      <div> 
+        <div> Main Page </div>
+        <Card />
+      </div>
+    )
+  }
+
+}
