fde58eb767dc05a1cfb615761d24c1c7962f8605
Tiffan Phan <thetiffanyphan@gmail.com>
2017-12-28 22:58:03 -0800

    more concise class setting for cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,14 +3,12 @@ import React, { Component } from 'react';
 class Card extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      classNames: `grid ${this.props.color}`
-    }
+    this.class = `grid ${this.props.color}`
   }
 
   render() {
     return (
-      <div className={this.state.classNames}></div>
+      <div className={this.class}></div>
     )
   }
 }
