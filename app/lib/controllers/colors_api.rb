a712a6a7dab87b64411b7b4d2f4fb94efceecbb2
Jonathan Liss <jonacom@lissismore.com>
2018-01-02 20:29:56 -0500

    add url for colored images

--- app/lib/controllers/colors_api.rb
+++ app/lib/controllers/colors_api.rb
@@ -2,6 +2,6 @@ class ColorsApi < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/colors/:color' do
-    { color: params[:color] }.to_json
+    { color: "http://via.placeholder.com/200x200/#{params[:color]}/#{params[:color]}" }.to_json
   end
 end
